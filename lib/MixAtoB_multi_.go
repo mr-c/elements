bdfd8643ecb3391270c8d08d5083641729d8db47
chr1sgrant <chrisrgrant@gmail.com>
2017-05-15 11:48:16 +0100

    update MixAtoB_multi

--- lib/MixAtoB_multi_.go
+++ lib/MixAtoB_multi_.go
@@ -1,7 +1,6 @@
-// Mix a list of ComponentsA to ComponentsB.
-// By default, each component in ComponentsB will have the component from the corresponding position in ComponentsA added to it
-// e.g.
-// ComponentsA [dna1,dna2] + ComponentsB [water, pbs] = MixedComponents [water + dna1, pbs + dna2]
+// The MixAtoB_multi element can be used to mix corresponding solutions from SolutionAs into SolutionBs,
+// For example, if mixing solution list A [dna1, dna2] to solution list B [water, pbs]
+// the resulting mixtures would be [water + dna1, pbs + dna2].
 package lib
 
 import
@@ -19,19 +18,21 @@ import
 
 // Parameters to this protocol
 
-// If a sample volume is specifed for a sample name contained  in ComponentsB, that volume of that component will be sampled.
-// if a "default" volume is specified that will be used as the sample volume for all components which do not have a value explicitely specified.
+// If a sample volume is specifed for a sample name contained  in SolutionBs, that volume of that component will be sampled.
+// If a "default" volume is specified that will be used as the sample volume for all components which do not have a value explicitely specified.
 // If no sample volume is specified for a component and no default set then the entire contents will be sampled.
 
 // Output data of this protocol
 
 // Physical inputs to this protocol
 
-// List of components to add to all components in ComponentsB
+// List of components to add to all components in SolutionBs.
 
-// Each component in ComponentsB will have the component from the corresponding position in ComponentsA added to it
+// Each solution in  the list of SolutionBs will have the component from the corresponding position in SolutionAs added to it.
 
 // Physical outputs to this protocol
+
+// Output solutions produced.
 
 // Conditions to run on startup
 func _MixAtoB_multiSetup(_ctx context.Context, _input *MixAtoB_multiInput) {
@@ -41,26 +42,33 @@ func _MixAtoB_multiSetup(_ctx context.Context, _input *MixAtoB_multiInput) {
 // The core process for this protocol. These steps are executed for each input.
 func _MixAtoB_multiSteps(_ctx context.Context, _input *MixAtoB_multiInput, _output *MixAtoB_multiOutput) {
 
-	if len(_input.ComponentsA) < len(_input.ComponentsB) {
-		execute.Errorf(_ctx, "not enough samples in ComponentsA to mix to all of ComponentsB: Found %d in ComponentsA, %d in ComponentsB", len(_input.ComponentsA), len(_input.ComponentsB))
+	// Check that there are enough solutions in SolutionBs for the number of solutions in SolutionAs.
+	if len(_input.SolutionBs) < len(_input.SolutionAs) {
+		execute.Errorf(_ctx, "Too many solutions listed in SolutionAs for the number of solutions in SolutionsB. Length of SolutionsB must be >= Length of SolutionsA. Found %d in SolutionAs, %d in SolutionBs", len(_input.SolutionAs), len(_input.SolutionBs))
 	}
 
-	for i := range _input.ComponentsB {
+	for i := range _input.SolutionAs {
 		var sample *wtype.LHComponent
 
 		var sampleVol wunit.Volume
-		if vol, found := _input.SampleVolumes[_input.ComponentsB[i].CName]; found {
+		// If a sample volume is specifed for a sample name contained  in SolutionAs, that volume of that component will be sampled.
+		if vol, found := _input.SampleVolumes[_input.SolutionAs[i].CName]; found {
 			sampleVol = vol
+			// If a "default" volume is specified that will be used as the sample volume for all components which do not have a value explicitely specified.
 		} else if vol, found := _input.SampleVolumes["default"]; found {
 			sampleVol = vol
 		}
 
+		// If no sample volume is specified and no default set, then the entire contents will be sampled.
+		// i.e. if after going through the map of sample volumes above the volume is greater than zero
+		// that volume will be sampled.
 		if sampleVol.RawValue() > 0.0 {
-			sample = mixer.Sample(_input.ComponentsA[i], sampleVol)
+			sample = mixer.Sample(_input.SolutionAs[i], sampleVol)
+			// if zero, then all the solution will be sampled
 		} else {
-			sample = mixer.SampleAll(_input.ComponentsA[i])
+			sample = mixer.SampleAll(_input.SolutionAs[i])
 		}
-		mixedComponent := execute.Mix(_ctx, _input.ComponentsB[i], sample)
+		mixedComponent := execute.Mix(_ctx, _input.SolutionBs[i], sample)
 		_output.MixedComponents = append(_output.MixedComponents, mixedComponent)
 
 	}
@@ -128,9 +136,9 @@ type MixAtoB_multiElement struct {
 }
 
 type MixAtoB_multiInput struct {
-	ComponentsA   []*wtype.LHComponent
-	ComponentsB   []*wtype.LHComponent
 	SampleVolumes map[string]wunit.Volume
+	SolutionAs    []*wtype.LHComponent
+	SolutionBs    []*wtype.LHComponent
 }
 
 type MixAtoB_multiOutput struct {
@@ -149,13 +157,13 @@ func init() {
 	if err := addComponent(component.Component{Name: "MixAtoB_multi",
 		Constructor: MixAtoB_multiNew,
 		Desc: component.ComponentDesc{
-			Desc: "Mix a list of ComponentsA to ComponentsB.\nBy default, each component in ComponentsB will have the component from the corresponding position in ComponentsA added to it\ne.g.\nComponentsA [dna1,dna2] + ComponentsB [water, pbs] = MixedComponents [water + dna1, pbs + dna2]\n",
+			Desc: "The MixAtoB_multi element can be used to mix corresponding solutions from SolutionAs into SolutionBs,\nFor example, if mixing solution list A [dna1, dna2] to solution list B [water, pbs]\nthe resulting mixtures would be [water + dna1, pbs + dna2].\n",
 			Path: "src/github.com/antha-lang/elements/an/Utility/Mix/MixAtoB_Multi.an",
 			Params: []component.ParamDesc{
-				{Name: "ComponentsA", Desc: "List of components to add to all components in ComponentsB\n", Kind: "Inputs"},
-				{Name: "ComponentsB", Desc: "Each component in ComponentsB will have the component from the corresponding position in ComponentsA added to it\n", Kind: "Inputs"},
-				{Name: "SampleVolumes", Desc: "If a sample volume is specifed for a sample name contained  in ComponentsB, that volume of that component will be sampled.\nif a \"default\" volume is specified that will be used as the sample volume for all components which do not have a value explicitely specified.\nIf no sample volume is specified for a component and no default set then the entire contents will be sampled.\n", Kind: "Parameters"},
-				{Name: "MixedComponents", Desc: "", Kind: "Outputs"},
+				{Name: "SampleVolumes", Desc: "If a sample volume is specifed for a sample name contained  in SolutionBs, that volume of that component will be sampled.\nIf a \"default\" volume is specified that will be used as the sample volume for all components which do not have a value explicitely specified.\nIf no sample volume is specified for a component and no default set then the entire contents will be sampled.\n", Kind: "Parameters"},
+				{Name: "SolutionAs", Desc: "List of components to add to all components in SolutionBs.\n", Kind: "Inputs"},
+				{Name: "SolutionBs", Desc: "Each solution in  the list of SolutionBs will have the component from the corresponding position in SolutionAs added to it.\n", Kind: "Inputs"},
+				{Name: "MixedComponents", Desc: "Output solutions produced.\n", Kind: "Outputs"},
 			},
 		},
 	}); err != nil {
