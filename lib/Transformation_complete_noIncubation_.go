5a35078ccafaef3976434d60db05f38373554286
chr1sgrant <chrisrgrant@gmail.com>
2017-06-21 18:18:22 +0100

    update single sample transformation elements

--- lib/Transformation_complete_noIncubation_.go
+++ lib/Transformation_complete_noIncubation_.go
@@ -16,7 +16,7 @@ import (
 
 //= 2 (hours)
 
-//Shakerspeed float64 // correct type?
+//Shakerspeed Rate
 
 //Plateoutdilution float64
 
@@ -37,36 +37,60 @@ func _Transformation_complete_noIncubationSetup(_ctx context.Context, _input *Tr
 // for every input
 func _Transformation_complete_noIncubationSteps(_ctx context.Context, _input *Transformation_complete_noIncubationInput, _output *Transformation_complete_noIncubationOutput) {
 
-	competentcells := make([]*wtype.LHComponent, 0)
+	var compCells *wtype.LHComponent
 
-	competentcells = append(competentcells, _input.CompetentCells)
+	_input.CompetentCells.SetVolume(_input.MinimumCompetentCellVolume)
 
-	// MixInto a specific plate e.g. plate ID blahblahblah001
-	readycompetentcellsComp := execute.MixInto(_ctx, _input.OutPlate, "", competentcells...) // readycompetentcells IS now a LHComponent
+	// find all wells on plate
+	allWells := _input.CompetentCellPlate.AllWellPositions(wtype.BYCOLUMN)
+
+	var cellsFound bool
+
+	// range through plate looking to see if the competent cells component is present
+	for i := range allWells {
+		if _input.CompetentCellPlate.WellMap()[allWells[i]].WContents.CName == _input.CompetentCells.CName && _input.CompetentCells.Volume().GreaterThan(_input.MinimumCompetentCellVolume) {
+			compCells = _input.CompetentCellPlate.WellMap()[allWells[i]].WContents
+			cellsFound = true
+			break
+		}
+	}
+	if !cellsFound {
+		// range through plate until an empty one is found
+		for i := range allWells {
+			if _input.CompetentCellPlate.WellMap()[allWells[i]].Empty() {
+				_input.CompetentCells.SetVolume(_input.MinimumCompetentCellVolume)
+				_input.CompetentCellPlate.WellMap()[allWells[i]].WContents = _input.CompetentCells
+				compCells = _input.CompetentCellPlate.WellMap()[allWells[i]].WContents
+				cellsFound = true
+				break
+			}
+		}
+	}
+
+	if !cellsFound {
+		execute.Errorf(_ctx, "No %s Cells found on plate with greater than %s volume and no empty positions", _input.CompetentCells.CName, _input.MinimumCompetentCellVolume.ToString())
+	} else {
+		execute.SetInputPlate(_ctx, _input.CompetentCellPlate)
+	}
+
+	readycompetentcellsComp := compCells // readycompetentcells IS now a LHComponent
 	/*
 		readycompetentcellsComp := Incubate (readycompetentcells,Preplasmidtemp, Preplasmidtime, false) // we can incubate an LHComponent so this is fine
 	*/
 
-	//competetentcellmix := mixer.Sample(readycompetentcellsComp, CompetentCellvolumeperassembly) // ERROR! mixer.Sample needs a liquid, not an LHComponent! however, the typeIIs method worked with a *LHComponent from inputs!
-	//transformationmix := make([]*LHComponent, 0)
-	//transformationmix = append(transformationmix, competetentcellmix)
 	DNAsample := mixer.Sample(_input.Reaction, _input.Reactionvolume)
-	//transformationmix = append(transformationmix, DNAsample)
 
 	transformedcellsComp := execute.Mix(_ctx, readycompetentcellsComp, DNAsample)
 
 	//transformedcellsComp := Incubate (transformedcells, Postplasmidtemp, Postplasmidtime,false)
 
-	//recoverymix := make([]*LHComponent, 0)
 	recoverymixture := mixer.Sample(_input.Recoverymedium, _input.Recoveryvolume)
 
-	//recoverymix = append(recoverymix,transformedcellsComp) // ERROR! transformedcells is now an LHComponent, not a liquid, so can't be used here
-	//recoverymix = append(recoverymix,recoverymixture)
 	recoverymix2Comp := execute.Mix(_ctx, transformedcellsComp, recoverymixture)
 
 	//recoverymix2Comp := Incubate (recoverymix2,  Recoverytemp, Recoverytime, true)
 
-	plateout := mixer.Sample(recoverymix2Comp, _input.Plateoutvolume) // ERROR! recoverymix2 is now an LHComponent, not a liquid, so can't be used here
+	plateout := mixer.Sample(recoverymix2Comp, _input.Plateoutvolume)
 	platedculture := execute.MixInto(_ctx, _input.AgarPlate, "", plateout)
 
 	_output.Platedculture = platedculture
@@ -132,21 +156,21 @@ type Transformation_complete_noIncubationElement struct {
 }
 
 type Transformation_complete_noIncubationInput struct {
-	AgarPlate                      *wtype.LHPlate
-	CompetentCells                 *wtype.LHComponent
-	CompetentCellvolumeperassembly wunit.Volume
-	OutPlate                       *wtype.LHPlate
-	Plateoutvolume                 wunit.Volume
-	Postplasmidtemp                wunit.Temperature
-	Postplasmidtime                wunit.Time
-	Preplasmidtemp                 wunit.Temperature
-	Preplasmidtime                 wunit.Time
-	Reaction                       *wtype.LHComponent
-	Reactionvolume                 wunit.Volume
-	Recoverymedium                 *wtype.LHComponent
-	Recoverytemp                   wunit.Temperature
-	Recoverytime                   wunit.Time
-	Recoveryvolume                 wunit.Volume
+	AgarPlate                  *wtype.LHPlate
+	CompetentCellPlate         *wtype.LHPlate
+	CompetentCells             *wtype.LHComponent
+	MinimumCompetentCellVolume wunit.Volume
+	Plateoutvolume             wunit.Volume
+	Postplasmidtemp            wunit.Temperature
+	Postplasmidtime            wunit.Time
+	Preplasmidtemp             wunit.Temperature
+	Preplasmidtime             wunit.Time
+	Reaction                   *wtype.LHComponent
+	Reactionvolume             wunit.Volume
+	Recoverymedium             *wtype.LHComponent
+	Recoverytemp               wunit.Temperature
+	Recoverytime               wunit.Time
+	Recoveryvolume             wunit.Volume
 }
 
 type Transformation_complete_noIncubationOutput struct {
@@ -169,9 +193,9 @@ func init() {
 			Path: "src/github.com/antha-lang/elements/an/Liquid_handling/Transformation/Transformation_noIncubation.an",
 			Params: []component.ParamDesc{
 				{Name: "AgarPlate", Desc: "", Kind: "Inputs"},
+				{Name: "CompetentCellPlate", Desc: "", Kind: "Inputs"},
 				{Name: "CompetentCells", Desc: "", Kind: "Inputs"},
-				{Name: "CompetentCellvolumeperassembly", Desc: "= 50.(uL)\n", Kind: "Parameters"},
-				{Name: "OutPlate", Desc: "", Kind: "Inputs"},
+				{Name: "MinimumCompetentCellVolume", Desc: "= 50.(uL)\n", Kind: "Parameters"},
 				{Name: "Plateoutvolume", Desc: "", Kind: "Parameters"},
 				{Name: "Postplasmidtemp", Desc: "", Kind: "Parameters"},
 				{Name: "Postplasmidtime", Desc: "", Kind: "Parameters"},
